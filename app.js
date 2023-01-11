c0fb5055a4768289c3cc6b851c31e28977bdd5e7
Wootthikrai <wootthikrai@buzzebees.com>
2023-01-11 15:30:23 +0700

    function

--- app.js
+++ app.js
@@ -64,6 +64,11 @@ app.post("/webhook", (req, res) => {
           req.body.entry[0].changes[0].value.metadata.phone_number_id;
         let from = req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
         let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
+        
+        if (msg_body == "redeem") {
+            console.log(myDateTime());
+        }
+        
         axios({
           method: "POST", // Required, HTTP method, a string, e.g. POST, GET
           url:
@@ -85,3 +90,7 @@ app.post("/webhook", (req, res) => {
       res.sendStatus(404);
     }
   });
+
+  app.myDateTime = function () {
+    return Date();
+  };
