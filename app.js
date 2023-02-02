493dba923456a8ec41e983f8c843f694dc1b5d71
Wootthikrai <wootthikrai@buzzebees.com>
2023-02-02 16:10:55 +0700

    req.body.entry

--- app.js
+++ app.js
@@ -55,13 +55,13 @@ app.post("/webhook", async (req, res) => {
   // Parse the request body from the POST
   let body = req.body;
   // Check the Incoming webhook message
-  console.log(JSON.stringify(req.body, null, 2));
+  // console.log(JSON.stringify(req.body, null, 2));
   const dadaJson = JSON.parse(body);
   await client.connect();
   console.log('Connected successfully to server');
   const db = client.db(dbName);
   const collection = db.collection('chats_data');
-  await collection.insertOne(dadaJson);
+  await collection.insertOne(req.body.entry);
   console.log('insertOne successfully to server');
   // info on WhatsApp text message payload: https://developers.facebook.com/docs/whatsapp/cloud-api/webhooks/payload-examples#text-messages
   if (req.body.object) {
