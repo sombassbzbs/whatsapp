9e829137fb92d7f046bebf0c653e48fe18dfca80
Wootthikrai <wootthikrai@buzzebees.com>
2023-02-02 14:49:20 +0700

    init db

--- app.js
+++ app.js
@@ -3,10 +3,19 @@ const token = process.env.WHATSAPP_TOKEN;
 
 // Imports dependencies and set up http server
 const request = require("request"),
-  express = require("express"),
-  body_parser = require("body-parser"),
-  axios = require("axios").default,
-  app = express().use(body_parser.json()); // creates express http server
+express = require("express"),
+body_parser = require("body-parser"),
+axios = require("axios").default,
+app = express().use(body_parser.json()); // creates express http server
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://sombass002:VcLYM0RAbO5D1AiK@cluster0.t3zaba5.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 app.get('/', (req, res) => {
   res.send('Hello Sombass!! 2002');
@@ -19,63 +28,67 @@ app.listen(port, () => {
 // Accepts GET requests at the /webhook endpoint. You need this URL to setup webhook initially.
 // info on verification request payload: https://developers.facebook.com/docs/graph-api/webhooks/getting-started#verification-requests
 app.get("/webhook", (req, res) => {
-    /**
-     * UPDATE YOUR VERIFY TOKEN
-     *This will be the Verify Token value when you set up webhook
-     **/
-    const verify_token = process.env.VERIFY_TOKEN;
+  /**
+  * UPDATE YOUR VERIFY TOKEN
+  *This will be the Verify Token value when you set up webhook
+  **/
+  const verify_token = process.env.VERIFY_TOKEN;
   
-    // Parse params from the webhook verification request
-    let mode = req.query["hub.mode"];
-    let token = req.query["hub.verify_token"];
-    let challenge = req.query["hub.challenge"];
+  // Parse params from the webhook verification request
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
   
-    // Check if a token and mode were sent
-    if (mode && token) {
-      // Check the mode and token sent are correct
-      if (mode === "subscribe" && token === verify_token) {
-        // Respond with 200 OK and challenge token from the request
-        console.log("WEBHOOK_VERIFIED");
-        res.status(200).send(challenge);
-      } else {
-        // Responds with '403 Forbidden' if verify tokens do not match
-        res.sendStatus(403);
-      }
+  // Check if a token and mode were sent
+  if (mode && token) {
+    // Check the mode and token sent are correct
+    if (mode === "subscribe" && token === verify_token) {
+      // Respond with 200 OK and challenge token from the request
+      console.log("WEBHOOK_VERIFIED");
+      res.status(200).send(challenge);
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
     }
-  });
+  }
+});
 
-  // Accepts POST requests at /webhook endpoint
+// Accepts POST requests at /webhook endpoint
 app.post("/webhook", (req, res) => {
-    // Parse the request body from the POST
-    let body = req.body;
-    // Check the Incoming webhook message
-    console.log(JSON.stringify(req.body, null, 2));
+  // Parse the request body from the POST
+  let body = req.body;
+  // Check the Incoming webhook message
+  console.log(JSON.stringify(req.body, null, 2));
   
-    // info on WhatsApp text message payload: https://developers.facebook.com/docs/whatsapp/cloud-api/webhooks/payload-examples#text-messages
-    if (req.body.object) {
-      if (
-        req.body.entry &&
-        req.body.entry[0].changes &&
-        req.body.entry[0].changes[0] &&
-        req.body.entry[0].changes[0].value.messages &&
-        req.body.entry[0].changes[0].value.messages[0]
+  const object = mongoose.model('Freelancer', FreelancersSchema)
+
+module.exports = Freelancers
+
+  // info on WhatsApp text message payload: https://developers.facebook.com/docs/whatsapp/cloud-api/webhooks/payload-examples#text-messages
+  if (req.body.object) {
+    if (
+      req.body.entry &&
+      req.body.entry[0].changes &&
+      req.body.entry[0].changes[0] &&
+      req.body.entry[0].changes[0].value.messages &&
+      req.body.entry[0].changes[0].value.messages[0]
       ) {
         let phone_number_id =
-          req.body.entry[0].changes[0].value.metadata.phone_number_id;
+        req.body.entry[0].changes[0].value.metadata.phone_number_id;
         let from = req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
         let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
         
         if (msg_body == "redeem") {
-            app.redeem(from, phone_number_id);
+          app.redeem(from, phone_number_id);
         }
         
         axios({
           method: "POST", // Required, HTTP method, a string, e.g. POST, GET
           url:
-            "https://graph.facebook.com/v12.0/" +
-            phone_number_id +
-            "/messages?access_token=" +
-            token,
+          "https://graph.facebook.com/v12.0/" +
+          phone_number_id +
+          "/messages?access_token=" +
+          token,
           data: {
             messaging_product: "whatsapp",
             to: from,
@@ -90,16 +103,16 @@ app.post("/webhook", (req, res) => {
       res.sendStatus(404);
     }
   });
-
+  
   app.redeem = function (from, phone_number_id) {
     console.log("SOMBASS LOG" + phone_number_id);
     axios({
       method: "POST", // Required, HTTP method, a string, e.g. POST, GET
       url:
-        "https://graph.facebook.com/v15.0/" +
-        phone_number_id +
-        "/messages?access_token=" +
-        token,
+      "https://graph.facebook.com/v15.0/" +
+      phone_number_id +
+      "/messages?access_token=" +
+      token,
       data: {
         messaging_product: "whatsapp",
         to: from,
