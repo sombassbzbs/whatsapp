c7a9418ae423dcbd301bab1c29f822e78dd71fdc
Wootthikrai <wootthikrai@buzzebees.com>
2023-02-06 16:54:09 +0700

    response

--- app.js
+++ app.js
@@ -80,8 +80,8 @@ app.post("/webhook", async (req, res) => {
         await app.saveChatLog(body); 
         
         const response = await app.deviceLogin(wa_id, phone_number_id); 
-        const jsonData =  JSON.stringify(response.data)
-        console.log(jsonData.token);
+        const jsonData = JSON.stringify(response.data)
+        console.log(jsonData);
         if (msg_body == "redeem") {
           app.redeem(from, phone_number_id);
         }
